69bcbf5dd076377884c1871cd58cef70753c40f8
Phil Morales <moralesphilip@gmail.com>
2020-03-25 16:02:40 -0400

    news fixes currently

---
+++ news-app/src/components/Countries/Noticias.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from "react";
+
+
+
+
+const Noticias = ({ item }) => (
+
+  //  <Fragment>
+ 
+  //     <div className="card">
+  //       <img src={item.urlToImage} className="card-img-top" alt="Main Pic" />
+  //       <div className="card-body">
+  //         <h5 className="card-title">{item.title}</h5>
+  //         <p className="card-text">{item.description}</p>
+  //       </div>
+  //       <ul className="list-group list-group-flush">
+  //         <li className="list-group-item">Author: {item.author}</li>
+  //         <li className="list-group-item">Source: {item.source.name}</li>
+  //       </ul>
+  //       <div className="card-body">
+  //         <a href={item.url} className="card-link">
+  //           Read More.
+  //         </a>
+  //       </div>
+  //     </div>
+   
+  // </Fragment>
+
+
+
+<Fragment>
+<div className="card">
+                <img
+                  src={item.urlToImage} onError="this.src='/images/default.jpg';" 
+                  className="card-img-top"
+                  alt="Article Picture"
+                  
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <h5 className="card-text">{item.author}</h5>
+
+                  <a
+                    href={item.url}
+                    className="btn btn-primary btn-lg btn-block"
+                    target= "_blank"
+                  >
+                    Read Me
+                  </a>
+                </div>
+              </div>
+
+
+</Fragment>
+
+);
+
+
+export default Noticias;
--- news-app/src/components/News/Main.js
+++ news-app/src/components/News/Main.js
@@ -1,35 +1,47 @@
 import React, { Component, Fragment } from 'react';
-import ArgentinaNews from "../Countries/Argentina";
+import Noticias from "../Countries/Noticias";
 
 
 class Main extends Component {
-    constructor() {
-        super();
 
+    constructor(props) {
+        super(props);
         this.state = {
-            newsData: [],
+            data: [],
         }
     }
 
     componentDidMount() {
-        const url = 'http://newsapi.org/v2/top-headlines?country=ar&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335';
+        const url = `https://newsapi.org/v2/${this.props.data.type}?${this.props.data.query}&category=business&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335`;
 
-        fetch(url)
-        .then(res => res.json())
-        .then(res => {
-            this.setState({
-                newsData: res.articles
-            });
-        })
-        .catch((error) => console.log(error));
+
+fetch(url)
+.then((res) => {
+    return res.json();
+})
+.then((data) => {
+    this.setState({
+        data: data.articles
+    })
+})
+.catch((err) => console.log(err));
     }
+
+
+    renderItems() {
+        return this.state.data.map((item) => (
+            <Noticias key={item.url} item={item} />
+        ));
+    }
+
 
     render() {
         return (
-            <Fragment>
-            <ArgentinaNews data={this.state.newsData} />
-            </Fragment>
+            <div className='row'>
+                {this.renderItems()}
+            </div>
         )
+         
     }
 }
 
